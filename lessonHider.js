e014ed0a6e6e57aa5412c6e038ca4b5b4e62fb84
Brian Hudson <brianjasonhudson@gmail.com>
2016-09-13 14:22:50 -0500

    "Added for loop to mark completed lessons"

--- lessonHider.js
+++ lessonHider.js
@@ -6,13 +6,20 @@ angular.module("directivePractice").directive("lessonHider", function(){
         lesson: "="
     }
     , templateUrl: 'lessonHider.html'
-    , link: function(scope, element, attributes) {
-        scope.getSchedule.then(function(result){
-          scope.schedule = result.data;
-        })
-    }
     , controller: function($scope, lessonService){
       $scope.getSchedule = lessonService.getSchedule();
+    }
+    , link: function(scope, element, attributes) {
+      scope.getSchedule.then(function(result){
+        scope.schedule = result.data;
+        console.log(scope.schedule);
+        for (var i = 0; i < scope.schedule.length; i++) {
+          if (scope.lesson === scope.schedule[i].lesson) {
+            element.css("text-decoration", "line-through");
+            return;
+          }
+        }
+      })
     }
   }
 });
